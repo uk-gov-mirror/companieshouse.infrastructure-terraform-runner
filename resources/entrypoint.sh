3a82e2aff6e14cedbde9c51a48b8540d7b414a87
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2026-02-17 08:54:16 +0000

    Restructure entrypoint, add debug logging and allow processing of the dependency lock file to be disabled via TF_RUNNER_IGNORE_DEPENDENCY_LOCK_FILE

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -12,20 +12,39 @@ if [[ ${TF_ARCHIVE} == "" ]]; then
     exit 1
 fi
 
-if [[ $UID -ne 0 ]]; then
-    log-output info "Preparing terraform environment"
-    sudo unzip -q ${TF_ARCHIVE} -d ${TF_BIN_PATH}/
-    sudo rsync -qa /root/.aws /home/tfrunner/
-    sudo rsync -qa /root/.ssh /home/tfrunner/
-    sudo rsync -qa --exclude '.terraform' /src /home/tfrunner/
-    sudo chown -R tfrunner:tfrunner /home/tfrunner/
-    pushd /home/tfrunner/src > /dev/null
-else
-    unzip -q ${TF_ARCHIVE} -d ${TF_BIN_PATH}/
+rsync_ignore_lock_opts=""
+if [[ -n "${TF_RUNNER_IGNORE_DEPENDENCY_LOCK_FILE}" ]] && [[ "${TF_RUNNER_IGNORE_DEPENDENCY_LOCK_FILE}" != "false" ]]; then
+    log-output info "Ignoring dependency lock file"
+    rsync_ignore_lock_opts="--exclude '.terraform.lock.hcl'"
 fi
+
+log-output info "Preparing terraform environment"
+
+unzip_command="sudo unzip -q ${TF_ARCHIVE} -d ${TF_BIN_PATH}/ terraform"
+log-output debug "Running unzip command: ${unzip_command}"
+eval "${unzip_command}"
+
+rsync_aws_command="sudo rsync -qa /root/.aws /home/tfrunner/"
+log-output debug "Running rsync command: ${rsync_aws_command}"
+eval "${rsync_aws_command}"
+
+rsync_ssh_command="sudo rsync -qa /root/.ssh /home/tfrunner/"
+log-output debug "Running rsync command: ${rsync_ssh_command}"
+eval "${rsync_ssh_command}"
+
+rsync_src_command="sudo rsync -qa --exclude '.terraform' ${rsync_ignore_lock_opts} /src /home/tfrunner/"
+log-output debug "Running rsync command: ${rsync_src_command}"
+eval "${rsync_src_command}"
+
+sudo chown -R tfrunner:tfrunner /home/tfrunner/
+pushd /home/tfrunner/src > /dev/null
+
+log-output debug "Passing operation to run-terraform"
 /usr/bin/run-terraform "$@"
 
-if [[ -n "${USER_UID}" ]] && [[ -n "${USER_GID}" ]]; then
-    log-output info "Syncing pending changes back to user"
-    sudo rsync -qa --chown=${USER_UID}:${USER_GID} --exclude '.git/' --exclude '.terraform/'  ./ /src
+if [[ -z $TF_RUNNER_IGNORE_DEPENDENCY_LOCK_FILE ]] || [[ "${TF_RUNNER_IGNORE_DEPENDENCY_LOCK_FILE}" == "false" ]]; then
+    if [[ -n "${USER_UID}" ]] && [[ -n "${USER_GID}" ]]; then
+        log-output info "Syncing pending changes back to user"
+        sudo rsync -qa --chown=${USER_UID}:${USER_GID} --exclude '.git/' --exclude '.terraform/'  ./ /src
+    fi
 fi
